890ae42c354e786088e83e813238ce2db0205f3b
Haley Huhtala <haley.huhtala@gmail.com>
2020-06-10 08:29:31 -0500

    updating before tutor

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_costa
+import scrape_mars
 
+import pymongo
+ 
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+# # Use PyMongo to establish Mongo connection
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+# # Setup connection to mongodb
+# conn = "mongodb://localhost:27017"
+# client = pymongo.MongoClient(conn)
+
+# # Select database and collection to use
+# db = client.mars_app
+# facts = db.facts
 
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
-    # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    # return("hi this is the homepage")
+    # # Find one record of data from the mongo database
+    # destination_data = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", facts)
 
 
 # Route that will trigger the scrape function
@@ -25,10 +36,15 @@ def home():
 def scrape():
 
     # Run the scrape function
-    costa_data = scrape_costa.scrape_info()
+    #mars_data = scrape_mars.scrape_info()
+    scrape_mars.scrape_info()
 
-    # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, costa_data, upsert=True)
+    # # Update the Mongo database using update and upsert=True
+    # mongo.db.collection.update({}, mars_data, upsert=True)
+
+    # write a statement that finds all the items in the db and sets it to a variable
+    inventory = list(facts.find())
+    print(inventory)
 
     # Redirect back to home page
     return redirect("/")
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,6 +1,14 @@
+# from splinter import Browser
+# from bs4 import BeautifulSoup as bs
+# import time
+
+# Dependencies
+from bs4 import BeautifulSoup
+import requests
+import pymongo
+
+import pandas as pd
 from splinter import Browser
-from bs4 import BeautifulSoup as bs
-import time
 
 
 def init_browser():
@@ -10,40 +18,81 @@ def init_browser():
 
 
 def scrape_info():
-    browser = init_browser()
 
-    # Visit visitcostarica.herokuapp.com
-    url = "https://visitcostarica.herokuapp.com/"
-    browser.visit(url)
+    #MARS FACTS
 
-    time.sleep(1)
+    url = "https://space-facts.com/mars/"
+    tables = pd.read_html(url)
+    mars_facts_df = tables[0]
+    html_table = mars_facts_df.to_html()
+    dict = {"fact_table": html_table}
+    dummy = 1
 
-    # Scrape page into Soup
-    html = browser.html
-    soup = bs(html, "html.parser")
 
-    # Get the average temps
-    avg_temps = soup.find('div', id='weather')
 
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
 
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
+    # browser = init_browser()
 
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
+    # # Visit visitcostarica.herokuapp.com
+    # url = "https://visitcostarica.herokuapp.com/"
+    # browser.visit(url)
 
-    # Store data in a dictionary
-    costa_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
-    }
+    # time.sleep(1)
 
-    # Close the browser after scraping
-    browser.quit()
+    # # Scrape page into Soup
+    # html = browser.html
+    # soup = bs(html, "html.parser")
 
-    # Return results
-    return costa_data
+    # # Get the average temps
+    # avg_temps = soup.find('div', id='weather')
+
+    # # Get the min avg temp
+    # min_temp = avg_temps.find_all('strong')[0].text
+
+    # # Get the max avg temp
+    # max_temp = avg_temps.find_all('strong')[1].text
+
+    # # BONUS: Find the src for the sloth image
+    # relative_image_path = soup.find_all('img')[2]["src"]
+    # sloth_img = url + relative_image_path
+
+    # # Store data in a dictionary
+    # costa_data = {
+    #     "sloth_img": sloth_img,
+    #     "min_temp": min_temp,
+    #     "max_temp": max_temp
+    # }
+
+    # # Close the browser after scraping
+    # browser.quit()
+
+    # # Return results
+    # return costa_data
+
+
+
+
+# TEST DB INSERT 
+
+import pymongo
+# Setup connection to mongodb
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+# Select database and collection to use
+db = client.mars_app
+facts = db.facts
+print("db set up")
+
+# Run the scrape function
+#MARS FACTS
+
+url = "https://space-facts.com/mars/"
+tables = pd.read_html(url)
+mars_facts_df = tables[0]
+html_table = mars_facts_df.to_html()
+dict = {"fact_table": html_table}
+
+facts.insert(dict)
+
+print("Data Uploaded!")
