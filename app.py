9a0fd28c7da9b2728fc454308da26d9cb5165634
Haley Huhtala <haley.huhtala@gmail.com>
2020-06-13 08:41:07 -0500

    webpage rough draft

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pymongo
 app = Flask(__name__)
 
 # # Use PyMongo to establish Mongo connection
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 # # Setup connection to mongodb
 # conn = "mongodb://localhost:27017"
@@ -19,16 +19,25 @@ app = Flask(__name__)
 # facts = db.facts
 
 
+# # Setup connection to mongodb
+# conn = "mongodb://localhost:27017"
+# client = pymongo.MongoClient(conn)
+
+# # Select database and collection to use
+# db = client.mars_app
+# facts = db.facts
+print("db set up")
+
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
     # return("hi this is the homepage")
     # # Find one record of data from the mongo database
-    # destination_data = mongo.db.collection.find_one()
+    mars_dict = mongo.db.facts.find_one()
 
     # Return template and data
-    return render_template("index.html", facts)
+    return render_template("index.html", mars_dict=mars_dict)
 
 
 # Route that will trigger the scrape function
@@ -36,15 +45,11 @@ def home():
 def scrape():
 
     # Run the scrape function
-    #mars_data = scrape_mars.scrape_info()
-    scrape_mars.scrape_info()
+    mars_dict = scrape_mars.scrape_info()
 
-    # # Update the Mongo database using update and upsert=True
-    # mongo.db.collection.update({}, mars_data, upsert=True)
-
-    # write a statement that finds all the items in the db and sets it to a variable
-    inventory = list(facts.find())
-    print(inventory)
+    # # Update the Mongo database
+    mongo.db.facts.update({}, mars_dict, upsert=True)
+    print("scrape done")
 
     # Redirect back to home page
     return redirect("/")
--- scrape_mars.py
+++ scrape_mars.py
@@ -9,24 +9,95 @@ import pymongo
 
 import pandas as pd
 from splinter import Browser
+from selenium import webdriver
+import time
 
 
 def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    #Path to the chromedriver
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    #executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
     return Browser("chrome", **executable_path, headless=False)
 
 
 def scrape_info():
 
-    #MARS FACTS
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=False)
+    
+    #NASA NEWS
+
+    url = 'https://mars.nasa.gov/news/'
+
+    browser.visit(url)
+    browser.is_element_present_by_css('li.slide', wait_time=10)
+
+    html = browser.html
+    
+    # Parse HTML with Beautiful Soup
+    soup = BeautifulSoup(html, 'html.parser')
+    article = soup.find('div', class_="article_teaser_body").text
+
+    title = soup.find(class_ ="bottom_gradient")
+    title = title.find('h3').text
+
+
+    #JPL MARS SPACE IMAGE
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, 'lxml')
+    footer = soup.find('footer')
+    string_footer = str(footer)
+    string_footer= string_footer.split('data-fancybox-href="')[1].split('" data-link')[0]
+    featured_image_url = 'https://www.jpl.nasa.gov' + string_footer
+
+    
+    # MARS FACTS
 
     url = "https://space-facts.com/mars/"
     tables = pd.read_html(url)
     mars_facts_df = tables[0]
     html_table = mars_facts_df.to_html()
-    dict = {"fact_table": html_table}
-    dummy = 1
+
+    # MARS HEMISHERES
+
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    html = browser.html
+    # Parse HTML with Beautiful Soup
+    soup = BeautifulSoup(html, 'html.parser')
+    hemispheres = soup.find_all('h3')
+    hemi_list = []
+    link_list = []
+
+    for h in hemispheres:
+        hemi_text = h.text.strip('Enhanced')
+        hemi_list.append(hemi_text)
+        #Click on Hemisphere Link
+        try:
+                browser.click_link_by_partial_text(hemi_text)
+        except:
+                print("Scraping Complete")
+        #Find image link
+        link = browser.find_link_by_text('Original').first['href']
+        link_list.append(link)
+        #Go Back a Page
+        browser.visit(url)
+
+    #Hard coding values scraped earlier when the site was working
+    hemi_list = ['Cerberus Hemisphere ', 'Schiaparelli Hemisphere ', 'Syrtis Major Hemisphere ', 'Valles Marineris Hemisphere ']
+    link_list = ['https://astrogeology.usgs.gov/cache/images/f5e372a36edfa389625da6d0cc25d905_cerberus_enhanced.tif_full.jpg', 'https://astrogeology.usgs.gov/cache/images/3778f7b43bbbc89d6e3cfabb3613ba93_schiaparelli_enhanced.tif_full.jpg', 'https://astrogeology.usgs.gov/cache/images/555e6403a6ddd7ba16ddb0e471cadcf7_syrtis_major_enhanced.tif_full.jpg', 'https://astrogeology.usgs.gov/cache/images/b3c7c6c9138f57b4756be9b9c43e3a48_valles_marineris_enhanced.tif_full.jpg']
+    hemi_dictionary = dict(zip(hemi_list, link_list))
+
+    #WRITE ALL TO DICT
+
+    mars_dict = {"article_title": title, "article_excerpt": article, "feature_image": featured_image_url, "fact_table": html_table, "hemisphere_pictures": hemi_dictionary, "hemisphere_list": hemi_list, "hemisphere_pic": link_list}
+
+
+    return(mars_dict)
 
 
 
@@ -74,25 +145,25 @@ def scrape_info():
 
 # TEST DB INSERT 
 
-import pymongo
-# Setup connection to mongodb
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
+# import pymongo
+# # Setup connection to mongodb
+# conn = "mongodb://localhost:27017"
+# client = pymongo.MongoClient(conn)
 
-# Select database and collection to use
-db = client.mars_app
-facts = db.facts
-print("db set up")
+# # Select database and collection to use
+# db = client.mars_app
+# facts = db.facts
+# print("db set up")
 
 # Run the scrape function
 #MARS FACTS
 
-url = "https://space-facts.com/mars/"
-tables = pd.read_html(url)
-mars_facts_df = tables[0]
-html_table = mars_facts_df.to_html()
-dict = {"fact_table": html_table}
+# url = "https://space-facts.com/mars/"
+# tables = pd.read_html(url)
+# mars_facts_df = tables[0]
+# html_table = mars_facts_df.to_html()
+# mars_dict = {"fact_table": html_table}
 
-facts.insert(dict)
+# facts.insert(mars_dict)
 
-print("Data Uploaded!")
+# print("Data Uploaded!")
